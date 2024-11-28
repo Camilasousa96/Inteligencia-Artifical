c3a9a8ef6e2c87155176d684c09793227ee6b129
Camilasousa96 <csilvatst@gmail.com>
2024-11-27 21:35:17 -0300

    Add files via upload

---
+++ Java/Dois_Collections.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Dois_Collections {
+    public static void main(String[] args) {
+        Collection<String> vogais = new ArrayList<>(); 
+        vogais.add("A");
+        vogais.add("E");
+        vogais.add("I");
+
+        Collection<String> vogais2 = Arrays.asList("O", "U");
+
+        //RECEBE TODOS OS ELEMENTOS DA COLEÇÃO ADCIONANDO NA COLEÇÃO VOGAIS
+        vogais.addAll(vogais2);
+
+        System.out.println("Lista das vogais:" +vogais);
+    }
+}
